0e998498a8b3737cdc8176c9c1927500f0f05e08
Shana Wiltshire <shanawiltshire@gmail.com>
2020-04-13 16:17:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,5 +29,15 @@ class Song
   def artist.count
     artist.count = Hash.new(0)
     @@artist {|artist| artist_count[artist]+=1}
+    artist_count
+  end 
+  
+  def genre.count
+    genre.count = Hash.new(0)
+    @@genre {|genre| genre_count[genre]+=1}
+    genre_count
+  end
+end
+
     
   
